d1740e43ca76eea3484d0a66c9c67e4c808dabe9
SergiuszStyczen <sergiusz.styczen@gmail.com>
2017-03-18 09:25:22 +0100

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#kontakt').click(function(){
+      $('footer').slideToggle();
+    });
+
+    $('#kontakt').click(function(){
+      $('html, body').animate({ scrollTop: 10000 }, 600);
+    });
+
+});
---
+++ js/modal.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+      $('#html').click(function(){
+        $('#htmlslide').toggle();
+      })
+
+      $('#css').click(function(){
+        $('#cssslide').toggle();
+      })
+
+      $('#js').click(function(){
+        $('#jsslide').toggle();
+      })
+
+      $('#jq').click(function(){
+        $('#jqslide').toggle();
+      })
+
+      $('#bs').click(function(){
+        $('#bsslide').toggle();
+      })
+
+      $('#git').click(function(){
+        $('#gitslide').toggle();
+      })
+});
